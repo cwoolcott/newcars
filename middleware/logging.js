0fbf75736242f988ef787dfcb1b13b5a4901fb04
cwoolcott <christopher.woolcott@gmail.com>
2024-05-09 21:35:04 -0500

    middleware fix

--- middleware/logging.js
+++ middleware/logging.js
@@ -3,9 +3,9 @@ const logging = (req, res, next) =>{
     console.log("MIDDLEWARE:", req.method)
     console.log('Request URL:', req.originalUrl);
 
-    if (true){
-        res.status(400).send("UH UH UH")
-    }
+    // if (true){
+    //     res.status(400).send("UH UH UH")
+    // }
     next();
 }
 
